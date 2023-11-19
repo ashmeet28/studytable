0bce013ef92ed70c8f0fb864dbece0470e13e8f6
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2023-11-19 09:47:36 -0500

    Merged sign in div with scene

--- src/SignInScene.jsx
+++ src/SignInScene.jsx
@@ -1,15 +1,32 @@
-import SignInDiv from './SignInDiv.jsx';
+import { useState } from 'react';
+
 import './SignInScene.scss';
 
 function SignInScene() {
-    function handleSignIn(u, p) {
-        console.log(u, p)
+    const [username, setUsername] = useState('');
+    const [userpassword, setPassword] = useState('');
+
+    function handleSignIn(e) {
+        e.preventDefault();
+        console.log(username, userpassword)
     }
+
 
     return (
         <div id='signinscene'>
             <h1>Study Table</h1>
-            <SignInDiv onSignIn={handleSignIn} />
+            <div id='usersignindiv'>
+                <form onSubmit={(e) => handleSignIn(e)}>
+                    <label htmlFor="usernameinput">Username</label><br></br>
+                    <input type="text" id='usernameinput' autoCapitalize='off' value={username} onChange={e => setUsername(e.target.value)} required /><br></br>
+
+                    <label htmlFor="passwordinput">Password</label><br></br>
+                    <input type="password" id='passwordinput' autoCapitalize='off' value={userpassword} onChange={e => setPassword(e.target.value)} required /><br></br>
+
+                    <input type="submit" value="Sign in" />
+                </form>
+                <button>Create a new organization</button>
+            </div>
         </div>
     )
 }
