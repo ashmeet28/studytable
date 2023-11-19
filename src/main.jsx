01e96cc8d23d76411715377244bfdabdcb0f2600
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2023-11-18 20:26:35 -0500

    Changed color

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import LoaderDiv from './LoaderDiv'
 
 ReactDOM.createRoot(document.getElementById('mainrootdiv')).render(
   <React.StrictMode>
-    <LoaderDiv />
+    {/* <LoaderDiv /> */}
     <SignInScene />
   </React.StrictMode>,
 )
